2964a40c366a68cbf91bd1cce82ae97431e18d9a
Nicolas FEVAL <nicolasfeval@gmail.com>
2020-03-30 14:07:19 +0200

    Ajout d'un constructeur à la classe Employee.java

--- Employee.java
+++ Employee.java
@@ -4,6 +4,14 @@ private String nom;
 private String prenom;
 private String email;
 private String telephone;
+private String adresse;
+
+/*
+ * ctor
+ */
 
 private int age;
+}
+
+public Employee(){
 }
